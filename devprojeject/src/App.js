4936e941ec8ab907281687c932c040a24f1cd7c2
Neeraj Moria <88096816+NeerajCap13@users.noreply.github.com>
2024-08-27 23:39:10 +0000

    HomeSection

--- devprojeject/src/App.js
+++ devprojeject/src/App.js
@@ -1,19 +1,13 @@
-import { BrowserRouter,Route, RouterProvider } from 'react-router-dom';
-import './App.css';
-import Login from './Component/Login';
-import Home from "./Component/Home"
+import "./App.css";
+
+import Home from "./Component/Home";
 
 function App() {
-
-  <BrowserRouter>
-      <Route exact path="/" component={Home} />
-      <Route path="/login" component={Login} />
-    </BrowserRouter>
   return (
     <>
-    <Home/>
+      <Home />
     </>
-  )
+  );
 }
 
 export default App;
