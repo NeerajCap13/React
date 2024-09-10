2eedb28de40ea5745af5f2fb2023a6071624a0b8
Neeraj Moria <88096816+NeerajCap13@users.noreply.github.com>
2024-09-10 14:29:09 +0000

    Up

--- devprojeject/src/Component/Landingpage/Landing.jsx
+++ devprojeject/src/Component/Landingpage/Landing.jsx
@@ -9,10 +9,9 @@ import {
   InputAdornment,
 } from "@mui/material";
 import React from "react";
-import loginImage from "../../Assets/images/login-illustrator.svg"
-import { ReactComponent as EmailIcon  } from "../../Assets/images/email.svg"
- import { ReactComponent as PassIcon  } from "../../Assets/images/pass.svg"
-
+import loginImage from "../../Assets/images/login-illustrator.svg";
+import { ReactComponent as EmailIcon } from "../../Assets/images/email.svg";
+import { ReactComponent as PassIcon } from "../../Assets/images/pass.svg";
 
 const Landing = () => {
   const theme = useTheme();
@@ -37,7 +36,6 @@ const Landing = () => {
               display: "flex",
               justifyContent: "center",
               alignItems: "center",
-              
             }}
           >
             <Box
@@ -47,11 +45,12 @@ const Landing = () => {
                 paddingBottom: "20px",
                 borderRadius: "5px",
                 textAlign: "center",
-                padding:"30px"
-                
+                padding: "30px",
               }}
             >
-              <Typography variant="h5" fontWeight={600}>DevLogix</Typography>
+              <Typography variant="h5" fontWeight={600}>
+                DevLogix
+              </Typography>
               <Typography variant="p" fontWeight={400}>
                 Login into your account
               </Typography>
@@ -61,19 +60,16 @@ const Landing = () => {
                 variant="outlined"
                 fullWidth
                 sx={{
-                  mt:"20px",
-                  mb:"10px",
+                  mt: "20px",
+                  mb: "10px",
                 }}
                 InputProps={{
-                  
                   endAdornment: (
-                    <InputAdornment position="end" >
-                      <EmailIcon/>
+                    <InputAdornment position="end">
+                      <EmailIcon />
                     </InputAdornment>
-                  )
-                  
+                  ),
                 }}
-                
               />
               <TextField
                 fullWidth
@@ -81,105 +77,129 @@ const Landing = () => {
                 label="Password"
                 variant="outlined"
                 InputProps={{
-                  
                   endAdornment: (
-                    <InputAdornment position="end" >
+                    <InputAdornment position="end">
                       <PassIcon />
                     </InputAdornment>
-                  )
-                  
+                  ),
                 }}
-                
               />
-              <Box textAlign={"right"}><Typography variant="p" color={"#1E2772"}>Forget Password</Typography></Box>
-                
-              <Button fullWidth sx={{
-                color:"white",
-                bgcolor:"#FD7401",
-                borderRadius:"8px",
-                mt:"20px",
-                "&:hover": { bgcolor: "#FD7401" },
-              }}>Login Now</Button>
-              <br/>
-             
-              
-              <br/><Divider sx={{fontSize:"10px" , fontWeight:"500"}}>OR</Divider><br/>
+              <Box textAlign={"right"}>
+                <Typography variant="p" color={"#1E2772"} fontSize={14}>
+                  Forget Password?
+                </Typography>
+              </Box>
 
-              <Button fullWidth 
-              sx={{
-                color:"#FD7401",
-                borderRadius:"8px",
-                border:"2px solid #FD7401 ",
-              }}>Signup Now</Button>
-              
+              <Button
+                fullWidth
+                sx={{
+                  color: "white",
+                  bgcolor: "#FD7401",
+                  borderRadius: "8px",
+                  mt: "20px",
+                  "&:hover": { bgcolor: "#FD7401" },
+                }}
+              >
+                Login Now
+              </Button>
+              <br />
+
+              <br />
+              <Divider sx={{ fontSize: "10px", fontWeight: "500" }}>OR</Divider>
+              <br />
+
+              <Button
+                fullWidth
+                sx={{
+                  color: "#FD7401",
+                  borderRadius: "8px",
+                  border: "2px solid #FD7401 ",
+                }}
+              >
+                Signup Now
+              </Button>
             </Box>
           </Box>
-          <Box  sx={{ height: "100%", width: "70vw" , bgcolor:"#C2C2C2", textAlign:"center"}}>
-           <img 
-           src={loginImage}
-           alt="imag"
-           
-            />
+          <Box
+            sx={{
+              height: "100%",
+              width: "70vw",
+              bgcolor: "#C2C2C2",
+              textAlign: "center",
+            }}
+          >
+            <img src={loginImage} alt="imag" />
           </Box>
         </>
       ) : (
-        <Box sx={{ height: "100%", width: "100vw", }}>
+        <Box sx={{ height: "100%", width: "100vw" }}>
           <Box
+            sx={{
+              maxWidth: "390px",
+              margin: "auto 20",
+              paddingBottom: "20px",
+              borderRadius: "5px",
+              textAlign: "center",
+              padding: "30px",
+            }}
+          >
+            <Typography variant="h5" fontWeight={700}>
+              DevLogix
+            </Typography>
+            <Typography variant="p" fontWeight={600}>
+              Login into your account
+            </Typography>
+            <TextField
+              id="outlined-basic"
+              label="Username"
+              variant="outlined"
+              fullWidth
               sx={{
-                maxWidth: "390px",
-                margin: "auto 20",
-                paddingBottom: "20px",
-                borderRadius: "5px",
-                textAlign: "center",
-                padding:"30px"
-                
+                mt: "20px",
+                mb: "10px",
+              }}
+            />
+            <TextField
+              fullWidth
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+            />
+            <Box textAlign={"right"}>
+              <Typography variant="p" color={"#1E2772"}>
+                Forget Password
+              </Typography>
+            </Box>
+
+            <Button
+              fullWidth
+              sx={{
+                color: "white",
+                bgcolor: "#FD7401",
+                borderRadius: "8px",
+                mt: "20px",
+                "&:hover": { bgcolor: "#FD7401" },
               }}
             >
-              <Typography variant="h5" fontWeight={700}>DevLogix</Typography>
-              <Typography variant="p" fontWeight={600}>
-                Login into your account
-              </Typography>
-              <TextField
-                id="outlined-basic"
-                label="Username"
-                variant="outlined"
-                fullWidth
-               
-                sx={{
-                  mt:"20px",
-                  mb:"10px",
-                  
-                }}
-              />
-              <TextField
-                fullWidth
-                id="outlined-basic"
-                label="Password"
-                variant="outlined"
-                
-              />
-              <Box textAlign={"right"}><Typography variant="p" color={"#1E2772"}>Forget Password</Typography></Box>
-                
-              <Button fullWidth sx={{
-                color:"white",
-                bgcolor:"#FD7401",
-                borderRadius:"8px",
-                mt:"20px",
-                "&:hover": { bgcolor: "#FD7401" },
-              }}>Login Now</Button>
-              <br/>
-             
+              Login Now
+            </Button>
+            <br />
 
-              <br/><Divider sx={{fontSize:"10px" , fontWeight:"500"}}>OR</Divider><br/>
+            <br />
+            <Divider sx={{ fontSize: "10px", fontWeight: "500" }}>OR</Divider>
+            <br />
 
-              <Button fullWidth 
+            <Button
+              fullWidth
               sx={{
-                color:"#FD7401",
-                borderRadius:"8px",
-                border:"2px solid #FD7401 ",
-              }}>Signup Now</Button>
-              
-            </Box>
+                color: "#FD7401",
+                borderRadius: "8px",
+                border: "2px solid #FD7401 ",
+              }}
+            >
+              Signup Now
+            </Button>
+          </Box>
         </Box>
       )}
     </Box>
